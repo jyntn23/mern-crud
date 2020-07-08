c00989ab3ff37c973d2fc0947cf263641ac22fb1
jyntn23 <52685048+jyntn23@users.noreply.github.com>
2020-07-09 02:38:34 +0800

    Login Component

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import {
   Collapse,
   Navbar,
@@ -6,20 +6,42 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText,
 } from "reactstrap";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 import RegisterModal from "./auth/RegisterModal";
+import LoginModal from "./auth/LoginModal";
 import Logout from "./auth/Logout";
 
 const AppNavbar = (props) => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const propTypes = {
+    auth: PropTypes.object.isRequired,
+  };
+
   const toggle = () => setIsOpen(!isOpen);
+
+  const { isAuthenticated, user } = this.props.auth;
+
+  const authLinks = (
+    <Fragment>
+      <NavItem>
+        <Logout />
+      </NavItem>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <NavItem>
+        <RegisterModal />
+      </NavItem>
+      <NavItem>
+        <LoginModal />
+      </NavItem>
+    </Fragment>
+  );
 
   return (
     <div>
@@ -28,29 +50,16 @@ const AppNavbar = (props) => {
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
-            <NavItem>
-              <RegisterModal />
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>Home Works</DropdownItem>
-                <DropdownItem>Anime</DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>Add List</DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-            <NavItem className="logout-btn">
-              <Logout />
-            </NavItem>
+            {isAuthenticated ? authLinks : guestLinks}
           </Nav>
-          <NavbarText>Organize your Lists</NavbarText>
         </Collapse>
       </Navbar>
     </div>
   );
 };
 
-export default AppNavbar;
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, null)(AppNavbar);
